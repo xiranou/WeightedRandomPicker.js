44552ee8e888a113ec4daf04b7d73617476ba046
xiranou <xiranou@gmail.com>
2015-11-13 12:44:18 -0500

    initial uniq option on randomPick

--- weightedRandomPicker.js
+++ weightedRandomPicker.js
@@ -5,11 +5,14 @@ function WeightedRandomPicker (initialWeightedValues) {
 
   this.randomPick = function(options){
     var options = options || {};
+    var uniq = options.uniq || false;
     var times = options.times || 1;
     var picks = [];
+
     for (var i=0; i<times; i++){
-      picks.push(_randomPick.call(this));
+      picks.push(_randomPick.call(this, uniq));
     }
+
     return options.times > 1 ? picks : picks[0];
   };
 
@@ -37,16 +40,33 @@ function WeightedRandomPicker (initialWeightedValues) {
     return Math.random() * max;
   };
 
-  var _randomPick = function(){
+  var _randomPick = function(uniq){
     var random = _getRandom.call(this);
     var pick;
     this.keys().forEach(function(key, index){
       var weight = this.weightedValues[key];
       if (weight >= random) {
         pick = pick || key; //only store the first pick
+        if (uniq && this.weightedValues[pick]) {
+          // delete from pick not from key since
+          // it is not breaking out from forEach
+          // also checks if the pick exists in
+          // the object
+          delete this.weightedValues[pick];
+        }
       }
       random -= weight;
     }.bind(this));
     return pick;
   };
 }
+
+var weightedValues = {"A": 0.1, "B": 0.7, "C": 0.7, "D": 0.8};
+
+var picker = new WeightedRandomPicker(weightedValues);
+
+var picks = picker.randomPick({times: 3});
+console.log(picks);
+
+var uniqPicks = picker.randomPick({times: 3, uniq: true});
+console.log(uniqPicks);
