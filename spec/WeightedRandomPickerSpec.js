9cecb9f5f11678a6f880f4946e0d26e72501bce0
xiranou <xiranou@gmail.com>
2015-11-13 14:51:15 -0500

    add specs on initiation

--- spec/WeightedRandomPickerSpec.js
+++ spec/WeightedRandomPickerSpec.js
@@ -3,4 +3,15 @@ describe('Can initiate', function() {
     var picker = new WeightedRandomPicker();
     expect(picker.weightedValues).toEqual({});
   });
+  it('should initiate with the given object', function() {
+    var weightedValues = {"A": 0.5, "B": 0.5, "C": 0.8};
+    var picker = new WeightedRandomPicker(weightedValues);
+    expect(picker.weightedValues).toEqual(weightedValues);
+  });
+  it('should initiate with a clone of the given object', function() {
+    var weightedValues = {"A": 0.5, "B": 0.5, "C": 0.8};
+    var picker = new WeightedRandomPicker(weightedValues);
+    picker.weightedValues["D"] = 1.0;
+    expect(picker.weightedValues).not.toEqual(weightedValues);
+  });
 });
